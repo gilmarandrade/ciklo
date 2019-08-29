0c111f8be710ba15465a85522629cf4f2ab1869c
Gilmar Andrade <gilmar-andrade@outlook.com>
2019-08-28 21:09:16 -0300

    cronometro atualizado em tempo real

--- demo2.js
+++ demo2.js
@@ -2,51 +2,30 @@ import Countdown from './countdown.js';
 
 const countdown = new Countdown('23 December 2019 23:59:59 GMT-0300');
 
-const container = document.querySelector('#countdow');
-
-// setInterval( () => {
-//     const time = countdown.toDays();
-//     container.innerHTML = `
-//         ${time.days} days
-//         ${time.hours} h
-//         ${time.minutes} m
-//         ${time.seconds} s
-//     `;
-// }, 1000);
-
-
+const counterSeconds = document.querySelector('#countdown-seconds');
+const counterMinutes = document.querySelector('#countdown-minutes');
+const counterHours = document.querySelector('#countdown-hours');
+const counterDays = document.querySelector('#countdown-days');
 
 setInterval( () => {
-    const counter = document.querySelector('#countdown-seconds'); 
-   updateCounterView(counter);
+    const time = countdown.toDays();
+    updateCounterView(counterSeconds, time.seconds);
+    updateCounterView(counterMinutes, time.minutes);
+    updateCounterView(counterHours, time.hours);
+    updateCounterView(counterDays, time.days);
+    
+    // console.log( `${time.days}days ${time.hours}h ${time.minutes}m ${time.seconds}s`);
 }, 1000);
 
-setInterval( () => {
-    const counter = document.querySelector('#countdown-minutes');
-    updateCounterView(counter);
- }, 2000);
-
- setInterval( () => {
-    const counter = document.querySelector('#countdown-hours');
-    updateCounterView(counter);
- }, 3000);
-
- setInterval( () => {
-    const counter = document.querySelector('#countdown-days');
-    updateCounterView(counter);
- }, 4000);
-
-function updateCounterView(counter) {
-    let index = +counter.dataset.time - 1;
-    counter.setAttribute('data-time', index);
-
-    counter.querySelector('span:first-child').innerText = index;
-    counter.classList.add('animate');
-
-    setTimeout(() => {
-        counter.querySelector('span:last-child').innerText = index;
-        //console.log(index);
-        counter.classList.remove('animate');
-        
-    }, 950);
+function updateCounterView(counter, indexNew) {
+    if(counter.getAttribute('data-time') != indexNew) {
+        counter.setAttribute('data-time', indexNew);
+        counter.querySelector('span:first-child').innerText = String(indexNew).padStart(2, '0');
+        counter.classList.add('animate');
+    
+        setTimeout(() => {
+            counter.querySelector('span:last-child').innerText = String(indexNew).padStart(2, '0');
+            counter.classList.remove('animate');
+        }, 950);
+    }
 }

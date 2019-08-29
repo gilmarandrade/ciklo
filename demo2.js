ecddba87823f14ba27f70f5eafdbf7e445760080
Gilmar Andrade <gilmar-andrade@outlook.com>
2019-08-28 21:24:03 -0300

    targetDate atualizada a partir de parametro recebido na queryString

--- demo2.js
+++ demo2.js
@@ -1,12 +1,30 @@
 import Countdown from './countdown.js';
 
-const countdown = new Countdown('23 December 2019 23:59:59 GMT-0300');
+//ler os parâmetros na query string e criar um array associativo
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+//inicializa o contador com a data recebina na query string, através dp parâmetro targetDate
+const countdown = new Countdown(urlParams.targetDate);//23 December 2019 23:59:59 GMT-0300
 
 const counterSeconds = document.querySelector('#countdown-seconds');
 const counterMinutes = document.querySelector('#countdown-minutes');
 const counterHours = document.querySelector('#countdown-hours');
 const counterDays = document.querySelector('#countdown-days');
 
+/**
+ * Atualiza cada campo do contador (dia, hora, minuto e segundo) a cada segundo
+ */
 setInterval( () => {
     const time = countdown.toDays();
     updateCounterView(counterSeconds, time.seconds);
@@ -17,6 +35,11 @@ setInterval( () => {
     // console.log( `${time.days}days ${time.hours}h ${time.minutes}m ${time.seconds}s`);
 }, 1000);
 
+/**
+ * Caso seja necessário, atualiza a tela com os novos dados do contador
+ * @param {Element} counter Elemento html que exibirá o valor
+ * @param {number} indexNew Valor a ser exibido no contador
+ */
 function updateCounterView(counter, indexNew) {
     if(counter.getAttribute('data-time') != indexNew) {
         counter.setAttribute('data-time', indexNew);

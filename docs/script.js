dff3cd86d991a483f40473c6f4112d60201f5286
Gilmar Andrade <gilmar-andrade@outlook.com>
2019-09-04 20:14:09 -0300

    pasta examples renomeada para docs

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-import Timer from './modules/timer.js';
+import Timer from '../modules/timer.js';
 
 
 const tempoParaONatal = new Timer('25 December ' + (new Date()).getFullYear() + ' 00:00:00 GMT-0300');
